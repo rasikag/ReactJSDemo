e8a67375da92a786b2a5ea73aa094ff72c0b8a78
rasikag <rasikagayan90@gmail.com>
2018-06-17 22:15:47 +0800

    Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Welcome to React JS. Shall we play a game?</h1>
+        <Person />
       </div>
     );
     //return React.createElement('div', null, 'h1', 'Hello from React app');
