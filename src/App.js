37cbf73dad9da1684874a34cb6b71979cbeb7cdd
rasikag <rasikagayan90@gmail.com>
2018-06-17 22:06:08 +0800

    JSX Restrictions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to React JS. Shall we play a game?</h1>
       </div>
     );
+    //return React.createElement('div', null, 'h1', 'Hello from React app');
+    //return React.createElement('div', null, 
+    //            React.createElement('h1',null, 'Hello from React Application'));
+    //return React.createElement('div',{class: 'App'}, 
+    //            React.createElement('h1',null, 'Hello from React Application'));
   }
 }
 
