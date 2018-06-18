bd99337693bd1b122034411806846ef1666ff2e0
rasikag <rasikagayan90@gmail.com>
2018-06-19 00:49:14 +0800

    Adding Two Way Binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>This is my name: {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed}/>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     );
 }
