00b838d31aed0e62dbfb373c48e3cd716799466c
rasikag <rasikagayan90@gmail.com>
2018-06-21 02:19:43 +0800

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ class App extends Component {
     if (this.state.showPerson) {
       persons = (
         <div>
+        {/*
           <Person name={this.state.persons[0].name} 
                   age={this.state.persons[0].age} 
                   changed={this.nameChangedHandler}/>
@@ -68,6 +69,13 @@ class App extends Component {
           </Person>
           <Person name={this.state.persons[2].name} 
                   age={this.state.persons[2].age} />
+        */}
+        {
+          this.state.persons.map(person => { 
+            return <Person name={person.name}
+                           age={person.age} />
+          })
+        }
         </div> 
       );
     }
